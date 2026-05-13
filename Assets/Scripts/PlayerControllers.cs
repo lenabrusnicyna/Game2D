4e8c55c1668d926a2fd05e925d4c5858d59a111d
lenabrusnicyna <118805689+lenabrusnicyna@users.noreply.github.com>
2026-05-13 16:32:39 +0500

    Kashapov

--- Assets/Scripts/PlayerControllers.cs
+++ Assets/Scripts/PlayerControllers.cs
@@ -2,15 +2,27 @@ using UnityEngine;
 
 public class PlayerControllers : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Rigidbody2D rb;
+    public float movespeed;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.linearVelocity = new Vector2(-movespeed, rb.linearVelocity.y);
+
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.linearVelocity = new Vector2(movespeed, rb.linearVelocity.y);
+
+        }
     }
 }
